8d48182837a927f0d21ec9b18a09cda46a0872ef
0xadev7 <leric7510@gmail.com>
2025-10-20 12:14:00 -0400

    fix: test product

--- tests/test_product.py
+++ tests/test_product.py
@@ -7,6 +7,7 @@ from checkerchain.miner.llm import (
 )
 from checkerchain.utils.checker_chain import fetch_product_data_with_score
 from checkerchain.validator.reward import reward
+from checkerchain.database.model import MinerPrediction
 import bittensor as bt
 
 miner_preds = {}
@@ -18,16 +19,28 @@ async def main():
     Uses a single OpenAI request per product to generate score, review, and keywords.
     """
     product_id = "684036c1a26d4eb551b68aff"
-    print(f"Fetching product data for {product_id}")
+    bt.logging.info(f"Fetching product data for {product_id}")
     product = fetch_product_data_with_score(product_id)
-    print("Product:\n", product)
+    bt.logging.info("Product:\n", product)
 
     # Generate assessments for found products
     response = await generate_complete_assessment(product)
-    print("Response:\n", response)
+    bt.logging.info("Response:\n", response)
 
-    task = await reward(None, response, product.trustScore, 0)
-    print("Reward:\n", task)
+    task = await reward(
+        None,
+        MinerPrediction(
+            id=0,
+            product_id=product_id,
+            miner_id=0,
+            prediction=response["score"],
+            review=response["review"],
+            keywords=response["keywords"],
+        ),
+        product.trustScore,
+        0,
+    )
+    bt.logging.info("Reward:\n", task)
 
 
 if __name__ == "__main__":
